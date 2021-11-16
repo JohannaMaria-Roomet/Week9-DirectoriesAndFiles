9e01c01d2d296d32b359533fac116667af6a02d4
Johanna Maria Roomet <jroomet@tktk.ee>
2021-11-16 22:03:23 +0200

    add new projects

---
+++ MyShoppingList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"D:\Users\...\samples\shoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add item to the shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your item:");
+                    string userShoppingList = Console.ReadLine();
+                    myShoppingList.Add(userShoppingList);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string item in myShoppingList)
+            {
+                Console.WriteLine($"Your item: {item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
